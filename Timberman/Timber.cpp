d7d6ff16e49a984888f215f321b5f21f98b804a1
Hamilton Gravito <hamiltongravito@gmail.com>
2025-05-20 10:08:12 -0300

    Animacao das nuvens e abelha.

--- Timberman/Timber.cpp
+++ Timberman/Timber.cpp
@@ -28,6 +28,43 @@ int main()
 	// Seta as coordenadas do spriteBackground nesse caso (0,0 - canto superior esquerdo)
     spriteBackground.setPosition(0,0);
 
+    // Preparando o troco de árvore
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    // Preparar abelha
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+    bool beeActive = false;
+    float beeSpeed = 0.0f;
+
+    // Preparar nuvens
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
+    Clock clock;
+    bool paused = true;
 
 	while (window.isOpen())
 	{
@@ -47,17 +84,86 @@ int main()
 		*/
 
 		// Verifica se a tecla ESC foio pressionada
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{
+		if (Keyboard::isKeyPressed(Keyboard::Escape)){
 			window.close();
 		}
+        if (Keyboard::isKeyPressed(Keyboard::Return)){
+            paused = false;
+        }
 
 		/*
 		****************************************
 		Update the scene
 		****************************************
 		*/
+        if(!paused){
+            Time dt = clock.restart();
+            if (!beeActive){
+                srand((int) time(0));
+                beeSpeed = (rand() % 100) + 100;
+                srand((int) time(0) * 10);
+                float height = (rand() % 500) + 500;
+                spriteBee.setPosition(2000, height);
+                beeActive = true;
+            } else {
+                spriteBee.setPosition(
+                    spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
+                if(spriteBee.getPosition().x < -100){
+                    beeActive = false;
+                }
+            }
 
+            if (!cloud1Active) {
+                srand((int)time(0) * 10);
+                cloud1Speed = (rand() % 200);
+                srand((int)time(0) * 10);
+                float height = (rand() % 150);
+                spriteCloud1.setPosition(-200, height);
+                cloud1Active = true;
+            } else {
+                spriteCloud1.setPosition(
+                    spriteCloud1.getPosition().x +
+                    (cloud1Speed * dt.asSeconds()),
+                    spriteCloud1.getPosition().y);
+                if (spriteCloud1.getPosition().x > 1920){
+                    cloud1Active = false;
+                }
+            }
+
+            if (!cloud2Active) {
+                srand((int)time(0) * 20);
+                cloud2Speed = (rand() % 200);
+                srand((int)time(0) * 20);
+                float height = ((rand() % 300) - 150);
+                spriteCloud2.setPosition(-200, height);
+                cloud2Active = true;
+            } else {
+                spriteCloud2.setPosition(
+                    spriteCloud2.getPosition().x +
+                    (cloud2Speed * dt.asSeconds()),
+                    spriteCloud2.getPosition().y);
+                if (spriteCloud2.getPosition().x > 1920){
+                    cloud2Active = false;
+                }
+            }
+
+            if (!cloud3Active) {
+                srand((int)time(0) * 30);
+                cloud3Speed = (rand() % 200);
+                srand((int)time(0) * 30);
+                float height = ((rand() % 450) - 150);
+                spriteCloud3.setPosition(-200, height);
+                cloud3Active = true;
+            } else {
+                spriteCloud3.setPosition(
+                    spriteCloud3.getPosition().x +
+                    (cloud3Speed * dt.asSeconds()),
+                    spriteCloud3.getPosition().y);
+                if (spriteCloud3.getPosition().x > 1920){
+                    cloud3Active = false;
+                }
+            }
+        }
 
 		/*
 		****************************************
@@ -68,8 +174,13 @@ int main()
 		// Apaga o quadro anterior
 		window.clear();
 
-		// Desenha a cena do game
+		// Desenha a cena do game (Cada sprite é desenhado na ordem de inserção)
 		window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
 
 		// Alterna entre a superfície exibida anteriormente (cena anterior) e a recém-atualizada (buffer duplo)
 		window.display();
